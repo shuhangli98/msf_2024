1c00deb3520375c1b1b50427e6ee001761e77c8f
Shuhang Li <shuhangli98@gmail.com>
2024-07-23 15:42:09 -0400

    pdb and xyz

--- msf_2024/io/pdb.py
+++ msf_2024/io/pdb.py
@@ -8,12 +8,16 @@ def open_pdb(f_loc):
     # This function reads in a pdb file and returns the atom names and coordinates.
     with open(f_loc) as f:
         data = f.readlines()
-    c = []
-    sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
+        
+    coords = []
+    symbols = []
+    
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            symbols.append(line[76:79].strip())
+            atom_coord = [float(x) for x in line[30:55].split()]
+            coords.append(atom_coord)
+    coords = np.array(coords)
+    symbols = np.array(symbols)
+    
+    return symbols, coords
--- msf_2024/io/xyz.py
+++ msf_2024/io/xyz.py
@@ -1,27 +1,37 @@
-'''
+"""
 functions for xyz files
-'''
+"""
 
 import numpy as np
 
+
 def open_xyz(file_location):
-    
+
     # Open an xyz file and return symbols and coordinates.
-    xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype='unicode')
-    symbols = xyz_file[:,0]
-    coords = (xyz_file[:,1:])
+    xyz_file = np.genfromtxt(fname=file_location, skip_header=2, dtype="unicode")
+    symbols = xyz_file[:, 0]
+    coords = xyz_file[:, 1:]
     coords = coords.astype(float)
     return symbols, coords
 
+
 def write_xyz(file_location, symbols, coordinates):
-    
+
     # Write an xyz file given a file location, symbols, and coordinates.
     num_atoms = len(symbols)
-    
-    with open(file_location, 'w+') as f:
-        f.write('{}\n'.format(num_atoms))
-        f.write('XYZ file\n')
-        
+
+    if len(symbols) != len(coordinates):
+        raise ValueError(
+            f"The number of symbols and coordinates must be the same: {len(symbols)} != {len(coordinates)} "
+        )
+
+    with open(file_location, "w+") as f:
+        f.write("{}\n".format(num_atoms))
+        f.write("XYZ file\n")
+
         for i in range(num_atoms):
-            f.write('{}\t{}\t{}\t{}\n'.format(symbols[i], 
-                                              coordinates[i,0], coordinates[i,1], coordinates[i,2]))
+            f.write(
+                "{}\t{}\t{}\t{}\n".format(
+                    symbols[i], coordinates[i, 0], coordinates[i, 1], coordinates[i, 2]
+                )
+            )
